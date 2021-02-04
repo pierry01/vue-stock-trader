079d457e02b58e5301c13e9ee4cd47093152a1c4
Jean Mendonça <pierrybm@gmail.com>
2021-02-04 11:51:40 -0300

    add buy button

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,21 +2,55 @@
   <v-flex class="pr-3 pb-3" xs12 md6 lg4>
     <v-card class="green darken-3 white--text">
       <v-card-title class="headline">
-        <strong>NOME <small>(Preço: PRECO)</small> </strong>
+        <strong>
+          {{ stock.name }}
+          <small>(Preço: {{ stock.price }})</small>
+        </strong>
       </v-card-title>
     </v-card>
 
     <v-card>
       <v-container fill-height>
-        <v-text-field label="Quantidade" type="Number" class="pr-3" />
-        <v-btn class="green darken-3 white--text"> Comprar </v-btn>
+        <v-text-field
+          v-model.number="quantity"
+          label="Quantidade"
+          type="Number"
+          class="pr-3"
+        />
+
+        <v-btn
+          class="green darken-3 white--text"
+          @click="buyStock"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+        >
+          Comprar
+        </v-btn>
       </v-container>
     </v-card>
   </v-flex>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0,
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity,
+      };
+
+      console.log(order);
+      this.quantity = 0;
+    },
+  },
+};
 </script>
 
 <style></style>
